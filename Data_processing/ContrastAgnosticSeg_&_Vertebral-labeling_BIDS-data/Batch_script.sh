cb3666b0761c93d26a6a9fa21b83c1f89252ee2c
Mathilde Brossard <mathilde@MacBookProKI.local>
2024-06-13 21:03:04 +0200

    Changes in files to check

--- Data_processing/ContrastAgnosticSeg_&_Vertebral-labeling_BIDS-data/Batch_script.sh
+++ Data_processing/ContrastAgnosticSeg_&_Vertebral-labeling_BIDS-data/Batch_script.sh
@@ -85,8 +85,8 @@ FILES_TO_CHECK=(
   "anat/${SUBJECT}_acq-mprage_T1w.nii_seg.nii.gz"
   "anat/${SUBJECT}_acq-mprage_T1w.nii_mask.nii.gz"
   "anat/${SUBJECT}_acq-mprage_T1w.nii_crop.nii.gz"
-  "anat/${SUBJECT}_acq-mprage_T1w.nii_seg_labeled.nii.gz"
-  "anat/${SUBJECT}_acq-mprage_T1w.nii_seg_labeled_discs.nii.gz"
+  "anat/label_vertebrae/${SUBJECT}_acq-mprage_T1w.nii_seg_labeled.nii.gz"
+  "anat/label_vertebrae/${SUBJECT}_acq-mprage_T1w.nii_seg_labeled_discs.nii.gz"
 )
 for file in ${FILES_TO_CHECK[@]}; do
   if [[ ! -e $file ]]; then
